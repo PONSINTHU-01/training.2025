bd6a690b7a5916e2e5c4674241b3cba012381407
PONSINTHU-01 <ponsindhu01@gmail.com>
2025-07-08 12:39:02 +0530

    Day 4

---
+++ fourth_day/compare.js
@@ -0,0 +1,20 @@
+let a=4;
+let b=8;
+let c=a+b;
+let d="s";
+if(a==4){
+    console.log("a is equal to 4")
+}
+else{
+    console.log("a is not equal to 4")
+}
+if(a===d)
+{
+    console.log(" true");
+}
+else{
+    console.log("flase");
+}
+let x=10;
+++x;
+console.log(x);
---
+++ fourth_day/var.js
@@ -0,0 +1,22 @@
+
+let x = 16 + "hi";
+console.log(x);
+let a=7;
+var b=8;
+if(true)
+{
+     let a=10;
+    console.log(a);
+}
+if(b=8)
+{
+    var b=5;
+}
+const cars = ["Saab", "Volvo", "BMW"];
+cars[0] = "Toyota";
+cars.push("Audi");
+console.log(cars);
+var y = 2;     // Allowed
+var y = 3;     // Allowed
+y= 4;
+console.log(y);
